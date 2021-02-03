c77b27caae82494dd055be544c9ed915b6b711b6
subin <subinshrestha429@gmail.com>
2021-02-03 20:04:05 +0545

    image loader added

--- Assignments/FinalProject/js/img/loadImages.js
+++
@@ -1,4 +0,0 @@
-function loadSprite() {
-	stage1 = new Image();
-	stage1.src = './images/stage1.png';
-}
--- Assignments/FinalProject/js/main.js
+++ Assignments/FinalProject/js/main.js
@@ -1,9 +1,25 @@
 import Game from './game.js';
+import { ryuSprite, kenSprite, loadScreen, KO, sfLogo, ryu_potrait, ken_potrait, chun_potrait, p1Select, p2Select } from './img/images.js';
+
+let images = [ryuSprite, kenSprite, loadScreen, KO, sfLogo, ryu_potrait, ken_potrait, chun_potrait, p1Select, p2Select];
+let imgCount = 0;
+let img = [];
+
+function loadImages() {
+	images.forEach((image) => {
+		image.onload = () => {
+			imgCount++;
+			if (imgCount === images.length) {
+				startGame();
+			}
+		};
+		img.push(image);
+	});
+}
 
 function startGame() {
-	// loadSprite();
 	let game = new Game('container', 'game');
 	game.init();
 }
 
-window.onload = startGame;
+loadImages();
