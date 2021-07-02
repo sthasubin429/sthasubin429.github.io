b8a809977883ad3a1ac0055315a1b3cc9f1c916e
subin <subinshrestha429@gmail.com>
2021-07-02 15:25:16 +0545

    Removed old Code

--- Assignments/JavaScript/Assignment-5/js/utility.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @param {Int} min Minimum Integer Value
- * @param {Int} max Maxmium Integer Value
- */
-const getRndInteger = (min, max) => {
-	return Math.floor(Math.random() * (max - min)) + min;
-};
